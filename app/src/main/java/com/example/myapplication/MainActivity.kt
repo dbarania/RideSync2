a76bcf656c501600d020ee0114e6874ed265fe92
Damian <damian@FlatLineDeb.something.com>
2023-07-09 09:28:09 +0200

    finished activityA

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,30 +1,78 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.GridView
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     private lateinit var lineGV:GridView
-    var nie:String = ""
+    var chosenRoute:String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        var routeList = readRoutesCSVData(applicationContext,"routes.csv")
         setContentView(R.layout.main_page_a)
         lineGV = findViewById(R.id.lines_grid)
 
-        val list = mutableListOf<String>("Nie", "lubic", "swiat")
-        val adapter = gridAdapter(this,list){x->nie = x
-        Log.d("MainActivity", "nie value: $nie")
+        val adapter = gridAdapter(this,routeList){x->chosenRoute = x
+        Log.d("MainActivity", "nie value: $chosenRoute")
         startNewActivity()
         }
         lineGV.adapter = adapter
     }
     private fun startNewActivity(){
         val intent = Intent(this,stops_b::class.java)
-        intent.putExtra("nieValue",nie) 
+        intent.putExtra("nieValue",chosenRoute)
         startActivity(intent)
     }
+}
+
+
+
+fun readStopCSVData(context: Context, fileName: String):Map<String?,Int?>{
+    val stopMap:MutableMap<String?,Int?> = mutableMapOf()
+    try {
+        val inputStream = context.assets.open(fileName)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        var line: String?
+
+        while (reader.readLine().also { line = it } != null) {
+            val values = line?.split(",")
+            val id = values?.get(0)?.toInt()
+            val stopName = values?.get(1).toString()
+            stopMap[stopName] = id
+        }
+
+        reader.close()
+        inputStream.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return stopMap
+}
+
+fun readRoutesCSVData(context: Context, fileName: String): List<String> {
+    val routeSet = mutableListOf<Int>()
+    try {
+        val inputStream = context.assets.open(fileName)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        var line: String?
+
+        while (reader.readLine().also { line = it } != null) {
+            line?.let { routeSet.add(it.toInt()) }
+        }
+        reader.close()
+        inputStream.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    routeSet.sort()
+    return routeSet.map { it.toString() }
+
 }
